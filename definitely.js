8a2eb0870eb4438616c01902d34dbf076f01c848
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-17 10:10:57 +0800

    Add files via upload

---
+++ definitely.js
@@ -0,0 +1,15 @@
+function Definitely(promise, times, onsuccess, onerror, delay) {
+  var runs = 0;
+  var errlog = [];
+  function run() {
+    promise.then(function() {
+      onsuccess.apply({}, [...arguments]);
+    }).catch(function(e) {
+      setTimeout(run, delay);
+      errlog.push(e);
+      runs++;
+    });
+    if(times => runs) onerror(errlog);
+  }
+  setTimeout(run, delay);
+}
